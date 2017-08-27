83d6f0d4e04b1939970314e0a632cc4f3a4de471
4cody <cshaner04@gmail.com>
2017-08-26 23:33:05 -0700

    fixes

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -18,14 +18,14 @@ $(document).on("click", ".animal", displayMovieInfo);
 $(".imgClick").on("click", function() {
   alert("yes");
   
-  // var state = $(this).attr("data-state");
-  // if (state === "still") {
-  //   $(this).attr("src", $(this).attr("data-animate"));
-  //   $(this).attr("data-state", "animate");
-  // } else {
-  //   $(this).attr("src", $(this).attr("data-still"));
-  //   $(this).attr("data-state", "still");
-  // }
+  var state = $(this).attr("data-state");
+  if (state === "still") {
+    $(this).attr("src", $(this).attr("data-animate"));
+    $(this).attr("data-state", "animate");
+  } else {
+    $(this).attr("src", $(this).attr("data-still"));
+    $(this).attr("data-state", "still");
+  }
 
 });
 
